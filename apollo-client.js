6f48475ef681c5e8f6e6a0b61614af45d15bdec5
LaurinJ <laurin223@seznam.cz>
2022-01-07 23:56:15 +0100

    apollo client configure

--- apollo-client.js
+++ apollo-client.js
@@ -1,20 +1,42 @@
 import { ApolloClient, InMemoryCache, split } from "@apollo/client";
 import { HttpLink } from "@apollo/client/link/http";
 import { getMainDefinition } from "@apollo/client/utilities";
+import { setContext } from "@apollo/client/link/context";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { useMemo } from "react";
+import { getCookie, getLocalStorage } from "./actions/auth";
 
 let apolloClient;
 
-const httpLink = new HttpLink({
+let httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql",
 });
+
+const authLink = setContext((_, { headers }) => {
+  // // get the authentication token from local storage if it exists
+  const token = getCookie("accessToken");
+  const chatId = getLocalStorage("chatId");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+      chatid: chatId ? chatId : "",
+    },
+  };
+});
+
+httpLink = authLink.concat(httpLink);
 
 const wsLink = process.browser
   ? new WebSocketLink({
       uri: "ws://localhost:4000/graphql",
       options: {
         reconnect: true,
+        connectionParams: {
+          authorization: `Bearer ${getCookie("accessToken")}`,
+          chatid: getLocalStorage("chatId"),
+        },
       },
     })
   : null;
