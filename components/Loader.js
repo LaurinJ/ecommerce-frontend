7e2c8c0c0f825ea31798ed6f9525d75b609b7e80
LaurinJ <laurin223@seznam.cz>
2022-03-02 22:37:34 +0100

    added Loader message

--- components/Loader.js
+++ components/Loader.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Loader() {
+function Loader({ message }) {
   return (
-    <div className="absolute w-full h-full flex items-center justify-center bg-gray-50 opacity-50 z-50">
-      <div className="w-20 h-20 border-t-2 border-l-2 border-b-2 border-green-900 rounded-full animate-spin"></div>
+    <div className="absolute w-full h-full flex flex-col items-center justify-center bg-gray-50 opacity-50 z-50">
+      <div className="w-20 h-20 border-t-4 border-l-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
+      <span className="text-lg font-medium">{message}</span>
     </div>
   );
 }
